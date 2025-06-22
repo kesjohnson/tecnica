eefed2ecb2d771041b4583d8a2dc1efcc43d4b02
Kes Johnson <70414636+kesjohnson@users.noreply.github.com>
2025-06-22 13:07:05 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/mi-api', methods=['POST'])
+def mi_funcion():
+    data = request.json
+    nombre = data.get('nombre', 'persona misteriosa')
+    return jsonify({'respuesta': f'¡Olé tú, {nombre}!'})
+
+if __name__ == '__main__':
+    app.run()
